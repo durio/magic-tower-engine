708788d3e40b7e481f178796a5dd2b2b4b1de492
durio <durio.thu@gmail.com>
2013-10-16 10:07:11 +0800

    Create new mixin ExtraInformation.

--- src/cn/edu/tsinghua/academic/c00740273/magictower/standard/mixin/ExtraInformation.java
+++ src/cn/edu/tsinghua/academic/c00740273/magictower/standard/mixin/ExtraInformation.java
@@ -11,22 +11,38 @@ import cn.edu.tsinghua.academic.c00740273.magictower.standard.RegularTileMixin;
 import cn.edu.tsinghua.academic.c00740273.magictower.standard.StandardEvent;
 import cn.edu.tsinghua.academic.c00740273.magictower.standard.StandardGame;
 
-public class Message implements RegularTileMixin {
+public class ExtraInformation implements RegularTileMixin {
 
 	private static final long serialVersionUID = 1L;
+
+	protected String key;
+	protected boolean add;
+	protected Object value;
 
 	@Override
 	public void initialize(JSONObject dataMixinValue) throws JSONException,
 			DataFormatException {
-		// TODO Auto-generated method stub
-
+		this.key = dataMixinValue.getString("key");
+		String action = dataMixinValue.getString("action");
+		if (action.equals("add")) {
+			this.add = true;
+		} else if (action.equals("set")) {
+			this.add = false;
+		} else {
+			throw new DataFormatException("Invalid action: " + action + ".");
+		}
+		this.value = dataMixinValue.get("value");
 	}
 
 	@Override
 	public boolean enter(StandardEvent event, Coordinate coord,
 			RegularTile tile, Coordinate sourceCoord, CharacterTile sourceTile,
 			StandardGame game) {
-		// TODO Auto-generated method stub
+		if (this.add) {
+			event.addExtraInformation(this.key, this.value);
+		} else {
+			event.setExtraInformation(this.key, this.value);
+		}
 		return true;
 	}
 
