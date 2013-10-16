70537b25a520571b201bdea7d0b29697041c45e4
durio <durio.thu@gmail.com>
2013-10-16 15:20:28 +0800

    Another DummyStandardRenderer.

---
+++ src/cn/edu/tsinghua/academic/c00740273/magictower/standard/DummyStandardRenderer.java
@@ -0,0 +1,17 @@
+package cn.edu.tsinghua.academic.c00740273.magictower.standard;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import cn.edu.tsinghua.academic.c00740273.magictower.engine.DummyRenderer;
+
+public class DummyStandardRenderer extends DummyRenderer implements
+		StandardRenderer {
+
+	@Override
+	public void initialize(JSONObject dataValue) throws JSONException,
+			DataFormatException {
+
+	}
+
+}
